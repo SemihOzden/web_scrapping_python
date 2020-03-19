8763f7faeb19b834471f1e40dfd1f3d3a51324f0
SemihOzden <semihozden01@gmail.com>
2020-03-20 00:45:32 +0300

    initial

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+import requests
+import re
+
+def run():
+    #url = "https://pcichecklist.com/"
+    url = input('please enter url:')
+
+    userText = input('What would like to search in '+url+":")
+
+    response = requests.get(url)
+    data = response.text
+    soup = BeautifulSoup(data, 'html.parser')
+    results = soup.body.find_all(string=re.compile('.*{0}.*'.format(userText)), recursive=True)
+
+    print('Found the word "{0}" {1} times\n'.format(userText, len(results)))
